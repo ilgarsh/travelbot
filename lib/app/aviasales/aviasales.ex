fe31cdcc91166f269764573c88244e2ab6854631
ilgarsh <ilgar.shahbazov@gmail.com>
2017-11-27 10:28:07 +0400

    add faster aviasales wrapper

--- lib/app/aviasales/aviasales.ex
+++ lib/app/aviasales/aviasales.ex
@@ -1,25 +1,39 @@
 defmodule Aviasales do
-  alias Proposal
 
-  @base_url "http://map.aviasales.ru/prices.json"
+  @token "70bbc8ece8117bb1e230fc89ae60e319"
+  @base_url "http://api.travelpayouts.com/v2/prices/latest"
 
-  def build_url(origin_iata, date, period, direct, min_trip_duration_in_days, max_trip_duration_in_days) do
+  def build_url(origin, destination, beginning_of_period,
+                  limit, trip_duration) do
     @base_url <> "?"
-     <> "origin_iata=" <> origin_iata <> "&"
-     <> "period=" <> date <> "&"
-     <> ":" <> period <> "&"
-     <> "direct=" <> direct <> "&" 
-     <> "&locale=ru&"
-     <> "min_trip_duration_in_days=" <> min_trip_duration_in_days <> "&"
-     <> "max_trip_duration_in_days=" <> max_trip_duration_in_days
+      <> if(origin != "", do: "origin=" <> origin <> "&", else: "")
+      <> if(destination != "", do: "destination=" <> destination <> "&", else: "")
+      <> if(beginning_of_period != "", do: "beginning_of_period=" <> beginning_of_period <> "&period_type=month&",
+                                        else: "period_type=year&")
+      <> "limit=" <> limit  <> "&"
+      <> if(trip_duration != "", do: "trip_duration=" <> trip_duration <> "&", else: "")
+      <> "token=" <> @token
   end
 
-  def get_proposals(origin_iata, date, period, direct, min_trip_duration_in_days, max_trip_duration_in_days) do
-    {:ok, response} = build_url(origin_iata, date, period, direct, 
-                          min_trip_duration_in_days, max_trip_duration_in_days) |>
-                      HTTPoison.get
-    Poison.decode!(response.body, as: [%Proposal{}]) |>
-    Enum.sort_by(&(&1.value))
+  def get_top_proposals(origin) do
+    {:ok, response} = build_url(origin, "", "", "10", "") |> HTTPoison.get
+    Poison.decode!(response.body, as: %{"data" => [%Proposal{}]})["data"]
+  end
+
+  def get_min_money_proposals(origin, money) do
+    {:ok, response} = build_url(origin, "", "", "1000", "") |> HTTPoison.get
+    Poison.decode!(response.body, as: %{"data" => [%Proposal{}]})["data"] |>
+    Enum.filter(fn prop -> prop.value < money end)
+  end
+
+  def get_proposals_to(origin, destination) do
+    {:ok, response} = build_url(origin, destination, "", "10", "") |> HTTPoison.get
+    Poison.decode!(response.body, as: %{"data" => [%Proposal{}]})["data"]
+  end
+
+  def get_proposals_in(origin, month) do
+    {:ok, response} = build_url(origin, "", "2017-" <> month <> "-01", "10", "") |> HTTPoison.get
+    Poison.decode!(response.body, as: %{"data" => [%Proposal{}]})["data"]
   end
 
 end
--- lib/app/aviasales/proposals.ex
+++ lib/app/aviasales/proposals.ex
@@ -3,13 +3,13 @@ defmodule Proposal do
 	@derive [Poison.Encoder]
 	defstruct [:show_to_affiliates,
 				:trip_class,
+				:origin,
 				:destination,
 				:depart_date,
 				:return_date,
 				:number_of_changes,
 				:value,
-				:created_at,
-				:ttl,
+				:found_at,
 				:distance,
 				:actual]
 
